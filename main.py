5dfcc8c43a582593f6bf1d642c1c18ec542964c9
CowinUnderwood23 <817281@mystma.org>
2021-10-06 14:29:32 -0500

    2

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 #WRITE YOUR CODE IN THIS FILE
+def factorial(x):
+    total = 1
+
+    for i in range(1, x + 1):
+        total = total * i
+        
+    
+    return total
+    i = i + 1 
+    
+
+        
+        
+        
+        
+
+print(factorial(10))
